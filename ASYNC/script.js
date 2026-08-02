b222576a4aed04193ed06a15f83974387bab905c
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-02 16:44:01 +0530

    Learned about CallBack and Promises

--- ASYNC/script.js
+++ ASYNC/script.js
@@ -1,11 +1,63 @@
-console.log("one");
-console.log("two");
+// console.log("one");
+// console.log("two");
 
 
-setTimeout(() =>{
-    console.log("Hello")
-}, 4000) // timeout
+// setTimeout(() =>{
+//     console.log("Hello")
+// }, 4000) // timeout
 
-console.log("three");
-console.log("four");
-console.log("five");
+// console.log("three");
+// console.log("four");
+// console.log("five");
+
+// function sum(a, b){
+//     console.log(a+b);
+// }
+
+// function calculator(a, b, sumCallback){
+//     sumCallback(a,b);
+// }
+
+// calculator (1,2,sum);
+
+// const hello = () =>{
+//     console.log("hello");
+// };
+
+// setTimeout(hello, 3000); 
+
+// let age = 19;
+
+// if(age > 18){
+//     if(age >= 60){
+//         console.log("senior");
+//     }
+//     else{
+//         console.log("Middle");
+//     }
+// }else{
+//  console.log("Child");
+// }
+
+let promise = new Promise((resolve, reject) =>{
+    console.log("I am a Promise");
+    // resolve("success");
+    reject("Some error");
+});
+// function getData(dataId, getNextData){//2S
+//     setTimeout(() =>{
+//         console.log("data = ", dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     }, 2000);
+// }
+
+// // CallBack Hell
+// getData(1, () =>{
+//     getData(2, () =>{
+//         getData(3, () =>{
+//             getData(4);
+//         });
+//     });
+// });
