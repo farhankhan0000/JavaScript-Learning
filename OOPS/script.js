de3afce39e12005e525ac06863fd6ca603863cc3
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-02 12:37:10 +0530

    learned about prototypes in objects

--- OOPS/script.js
+++ OOPS/script.js
@@ -1,8 +1,39 @@
-const student = {
-    fullName: "Farhan Khan",
-    age: 20,
-    marks: 94.4,
-    printMarks : function () {
-        console.log("marks = ", this.marks);
-    },
+// const student = {
+//     fullName: "Farhan Khan",
+//     age: 20,
+//     marks: 94.4,
+//     printMarks : function () {
+//         console.log("marks = ", this.marks);
+//     },
+// };
+
+const employee = {
+    calcTax() {
+        console.log("Tax rate is 10%");
+    }
 };
+
+const karanArjun1 = {
+    salary: 50000,
+}
+
+const karanArjun2 = {
+    salary: 50000,
+    calcTax() {
+        console.log("Tax rate is 20%");
+    }
+}
+
+const karanArjun3 = {
+    salary: 50000,
+}
+
+const karanArjun4 = {
+    salary: 50000,
+}
+
+
+karanArjun1.__proto__ = employee;
+karanArjun2.__proto__ = employee;
+karanArjun3.__proto__ = employee;
+karanArjun4.__proto__ = employee;
