8e2d6be66fd3f377bfa5311ee496620edd37ed28
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-02 13:04:20 +0530

    learned about classes , setters in classes and constructors in classes

--- OOPS/script.js
+++ OOPS/script.js
@@ -7,33 +7,54 @@
 //     },
 // };
 
-const employee = {
-    calcTax() {
-        console.log("Tax rate is 10%");
+// const employee = {
+//     calcTax() {
+//         console.log("Tax rate is 10%");
+//     }
+// };
+
+// const karanArjun1 = {
+//     salary: 50000,
+// }
+
+// const karanArjun2 = {
+//     salary: 50000,
+//     calcTax() {
+//         console.log("Tax rate is 20%");
+//     }
+// }
+
+// const karanArjun3 = {
+//     salary: 50000,
+// }
+
+// const karanArjun4 = {
+//     salary: 50000,
+// }
+
+
+// karanArjun1.__proto__ = employee;
+// karanArjun2.__proto__ = employee;
+// karanArjun3.__proto__ = employee;
+// karanArjun4.__proto__ = employee;
+
+
+class ToyotaCar{
+    constructor(brand, mileage){
+        console.log("Creating new object");
+        this.brand = brand;
+        this.mileage = mileage;
     }
-};
+    start(){
+        console.log("start");
+    }
 
-const karanArjun1 = {
-    salary: 50000,
-}
-
-const karanArjun2 = {
-    salary: 50000,
-    calcTax() {
-        console.log("Tax rate is 20%");
+    stop(){
+        console.log("stop");
     }
 }
 
-const karanArjun3 = {
-    salary: 50000,
-}
-
-const karanArjun4 = {
-    salary: 50000,
-}
-
-
-karanArjun1.__proto__ = employee;
-karanArjun2.__proto__ = employee;
-karanArjun3.__proto__ = employee;
-karanArjun4.__proto__ = employee;
+let fortuner = new ToyotaCar("fortuner", 10);
+console.log(fortuner);
+let lexus = new ToyotaCar("lexus", 12);
+console.log(lexus);
