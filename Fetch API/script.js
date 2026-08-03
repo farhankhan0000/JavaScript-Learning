74af681cd797c220f4a33b96ec6f0c284d835d3d
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-03 18:16:54 +0530

    making currency converter

--- Fetch API/script.js
+++ Fetch API/script.js
@@ -1,14 +1,28 @@
-const URL = "https://dogapi.dog/api/v2/breeds";
-const breedName = document.querySelector("#name");
-const btn = document.querySelector("#btn");
+// const URL = "https://dogapi.dog/api/v2/breeds";
+// const breedName = document.querySelector("#name");
+// const btn = document.querySelector("#btn");
 
-const getFacts = async () =>{
-    console.log("getting data.....");
-    let response = await fetch(URL);
-    console.log(response); //JSON Format
-    let data = await response.json();
-    //console.log(data.data[0]);
-    breedName.innerText = data.data[0].attributes.name;
-};
+// const getFacts = async () =>{
+//     console.log("getting data.....");
+//     let response = await fetch(URL);
+//     console.log(response); //JSON Format
+//     let data = await response.json();
+//     //console.log(data.data[0]);
+//     breedName.innerText = data.data[2].attributes.name;
+// };
 
-btn.addEventListener("click", getFacts);
+// // function getFacts(){
+// //     fetch(URL).then((response) => {
+// //         return response.json();
+// //     }).then((data) => {
+// //         console.log(data);
+// //         breedName.innerText = data.data[0].attributes.name;
+// //     });
+// // };
+
+// btn.addEventListener("click", getFacts);
+
+let x = fetch("https://dogapi.dog/api/v2/breeds");
+console.log(x);
+console.log(typeof x);
+console.log(x instanceof Promise);
